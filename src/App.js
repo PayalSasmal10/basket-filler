a0656462b8f68eae3b1d7db1598ac7d09061deb0
Payal Sasmal <sasmalpayal@gmail.com>
2023-09-24 21:39:42 +0530

    basket filler is created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+import { useState } from "react";
 
 function App() {
+  const arr = ["item1", "item2", "item3", "item4", "item5"];
+  const [basket, setBasket] = useState([]);
+
+  const addingItemsAfterClicking = (item) => {
+    if (basket.includes(item)) {
+      setBasket(basket.filter((basket_item) => basket_item !== item));
+    } else {
+      setBasket([...basket, item]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        {arr.map((item) => {
+          return (
+            <ul key={item}>
+              <li onClick={() => addingItemsAfterClicking(item)}>{item}</li>
+            </ul>
+          );
+        })}
+      </div>
+      <div>
+        <h2>Basket</h2>
+        {basket.map((item) => (
+          <ul key={item}>
+            <li>{item}</li>
+          </ul>
+        ))}
+      </div>
+    </>
   );
 }
 
